c393f0d57df8ae30b9fa5c81e3905da6b581d8f5
cbl <lennart.carbe@gmail.com>
2021-10-01 13:47:42 +0200

    fix indexjs

--- packages/macrame-vue3/index.js
+++ packages/macrame-vue3/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./dist/compiler-core.cjs.prod.js');
+    module.exports = require('./dist/macrame-vue3.cjs.prod.js');
 } else {
-    module.exports = require('./dist/compiler-core.cjs.js');
+    module.exports = require('./dist/macrame-vue3.cjs.js');
 }
