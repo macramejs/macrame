6ad14d88a28b9d9afef1604f10acd8193327aa6d
cbl <lennart.carbe@gmail.com>
2022-01-31 10:01:55 +0100

    housekeeping

--- packages/macrame-vue3/src/index.ts
+++ packages/macrame-vue3/src/index.ts
@@ -2,5 +2,4 @@
 export { default as useIndex } from './useIndex';
 export { default as useForm } from './useForm';
 export { default as useList } from './useList';
-export { default as Input } from './components/Input';
-export { default as List } from './components/List';
+export { default as Input } from './input';
