469c951336347ab4b4a41c5aa62545c87e2128cd
cbl <lennart.carbe@gmail.com>
2022-08-09 15:57:44 +0200

    release: v0.3.23

--- packages/macrame-vue3/src/useIndex.ts
+++ packages/macrame-vue3/src/useIndex.ts
@@ -94,10 +94,8 @@ const useIndex: UseIndex = function useIndex({
         },
         removeSortBy(key, reload = true) {
             const sorting = clone(this.sortBy);
-            console.log(sorting);
             for (let i = 0; i < sorting.length; i++) {
-                console.log(i, sorting);
-                if (sorting[i].key == key) delete this.sortBy[i];
+                if (sorting[i] && sorting[i].key == key) delete this.sortBy[i];
             }
 
             if (reload) this.load();
